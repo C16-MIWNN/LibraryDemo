de33c5976c2f4408cc569dfaac1105754ca26aa2
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2025-06-05 15:05:49 +0200

    Starter project from Spring initializer

---
+++ src/test/java/nl/miwnn/ch16/vincent/librarydemo/LibraryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch16.vincent.librarydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
