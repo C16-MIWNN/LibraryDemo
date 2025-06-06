3b9119748cd710472e88af6b4163c7477262db0d
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2025-06-06 10:04:57 +0200

    Add the ability to add books through a form

--- src/main/java/nl/miwnn/ch16/vincent/librarydemo/controller/BookController.java
+++ src/main/java/nl/miwnn/ch16/vincent/librarydemo/controller/BookController.java
@@ -1,9 +1,13 @@
 package nl.miwnn.ch16.vincent.librarydemo.controller;
 
+import nl.miwnn.ch16.vincent.librarydemo.model.Book;
 import nl.miwnn.ch16.vincent.librarydemo.repositories.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
 
@@ -20,11 +24,28 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
-    @GetMapping("/")
+    @GetMapping({"/", "/book/overview"})
     private String showBookOverview(Model datamodel) {
         datamodel.addAttribute("allBooks", bookRepository.findAll());
 
         return "bookOverview";
     }
 
+    @GetMapping("/book/new")
+    private String showNewBookForm(Model datamodel) {
+        datamodel.addAttribute("formBook", new Book());
+
+        return "bookForm";
+    }
+
+    @PostMapping("/book/save")
+    private String saveOrUpdateBook(@ModelAttribute("formBook") Book bookToBeSaved, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            System.err.println(bindingResult.getAllErrors());
+        } else {
+            bookRepository.save(bookToBeSaved);
+        }
+
+        return "redirect:/book/overview";
+    }
 }
