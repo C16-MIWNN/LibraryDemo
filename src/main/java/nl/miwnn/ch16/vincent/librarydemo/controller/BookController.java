99107f3139b307151c95f992dad214be0083f4d5
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2025-06-06 10:11:15 +0200

    Add the ability to delete books from the overview page

--- src/main/java/nl/miwnn/ch16/vincent/librarydemo/controller/BookController.java
+++ src/main/java/nl/miwnn/ch16/vincent/librarydemo/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
@@ -46,6 +47,12 @@ public class BookController {
             bookRepository.save(bookToBeSaved);
         }
 
+        return "redirect:/book/overview";
+    }
+
+    @GetMapping("/book/delete/{bookId}")
+    private String deleteBook(@PathVariable("bookId") Long bookId) {
+        bookRepository.deleteById(bookId);
         return "redirect:/book/overview";
     }
 }
