311d95816fad49f72ac61cf56127e05e5aad3a44
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2025-06-11 15:10:10 +0200

    Books are now in cards, copy functionality currently broken

--- src/main/java/nl/miwnn/ch16/vincent/librarydemo/controller/BookController.java
+++ src/main/java/nl/miwnn/ch16/vincent/librarydemo/controller/BookController.java
@@ -6,12 +6,14 @@ import nl.miwnn.ch16.vincent.librarydemo.repositories.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 /**
  * @author Vincent Velthuizen
@@ -28,11 +30,38 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
-    @GetMapping({"/", "/book/overview"})
-    private String showBookOverview(Model datamodel) {
+    private String setupBookOverview(Model datamodel, Book formBook, boolean formModalHidden) {
         datamodel.addAttribute("allBooks", bookRepository.findAll());
+        datamodel.addAttribute("formBook", formBook);
+        datamodel.addAttribute("allAuthors", authorRepository.findAll());
+        datamodel.addAttribute("formModalHidden", formModalHidden);
 
         return "bookOverview";
+    }
+
+    private String setupBookDetail(Model datamodel, Book bookToShow, Book formBook, boolean formModalHidden) {
+        datamodel.addAttribute("book", bookToShow);
+        datamodel.addAttribute("formBook", formBook);
+        datamodel.addAttribute("allAuthors", authorRepository.findAll());
+        datamodel.addAttribute("formModalHidden", formModalHidden);
+
+        return "bookDetails";
+    }
+
+    @GetMapping({"/", "/book/overview"})
+    private String showBookOverview(Model datamodel) {
+        return setupBookOverview(datamodel, new Book(), true);
+    }
+
+    @GetMapping("/book/detail/{title}")
+    private String showBookDetailPage(@PathVariable("title") String title, Model datamodel) {
+        Optional<Book> bookOptional = bookRepository.findByTitle(title);
+
+        if (bookOptional.isEmpty()) {
+            return "redirect:/book/overview";
+        }
+
+        return setupBookDetail(datamodel, bookOptional.get(), bookOptional.get(), true);
     }
 
     @GetMapping("/book/new")
@@ -44,19 +73,30 @@ public class BookController {
     }
 
     @PostMapping("/book/save")
-    private String saveOrUpdateBook(@ModelAttribute("formBook") Book bookToBeSaved, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            System.err.println(bindingResult.getAllErrors());
-        } else {
-            bookRepository.save(bookToBeSaved);
+    private String saveBook(@ModelAttribute("formBook") Book bookToBeSaved, BindingResult result, Model datamodel) {
+        checkBookTitleExists(bookToBeSaved, result);
+
+        if (result.hasErrors() && bookToBeSaved.getBookId() != null) {
+            Book originalBookDetails = bookRepository.findById(bookToBeSaved.getBookId()).orElse(new Book());
+            return setupBookDetail(datamodel, originalBookDetails, bookToBeSaved, false);
+        } else if (result.hasErrors()) {
+            return setupBookOverview(datamodel, bookToBeSaved, false);
         }
 
-        return "redirect:/book/overview";
+        bookRepository.save(bookToBeSaved);
+        return "redirect:/book/detail/" + bookToBeSaved.getTitle();
     }
 
-    @GetMapping("/book/delete/{bookId}")
-    private String deleteBook(@PathVariable("bookId") Long bookId) {
-        bookRepository.deleteById(bookId);
+    private void checkBookTitleExists(Book bookToBeSaved, BindingResult result) {
+        Optional<Book> sameName = bookRepository.findByTitle(bookToBeSaved.getTitle());
+        if (sameName.isPresent() && !sameName.get().getBookId().equals(bookToBeSaved.getBookId())) {
+            result.addError(new FieldError("book", "title", "this title is already in use"));
+        }
+    }
+
+    @GetMapping("/book/delete/{bookTitle}")
+    private String deleteBook(@PathVariable("bookTitle") String title) {
+        bookRepository.findByTitle(title).ifPresent(bookRepository::delete);
         return "redirect:/book/overview";
     }
 }
--- src/main/java/nl/miwnn/ch16/vincent/librarydemo/repositories/BookRepository.java
+++ src/main/java/nl/miwnn/ch16/vincent/librarydemo/repositories/BookRepository.java
@@ -1,7 +1,11 @@
 package nl.miwnn.ch16.vincent.librarydemo.repositories;
 
+import nl.miwnn.ch16.vincent.librarydemo.model.Author;
 import nl.miwnn.ch16.vincent.librarydemo.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByTitle(String title);
 }
